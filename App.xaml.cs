d50089ab0a3f6c16b13679b640f2c91c47b1ad2d
Crisan Iulia <crisaniuliaa21@gmail.com>
2022-12-06 20:37:32 +0200

    Lab7

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,30 @@
-﻿namespace Crisan_Iulia_Lab7;
+﻿using System;
+using Crisan_Iulia_Lab7.Data;
+using System.IO;
 
-public partial class App : Application
+
+namespace Crisan_Iulia_Lab7
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+    }
 }
